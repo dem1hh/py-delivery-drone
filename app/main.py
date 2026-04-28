84f975cbfc8014f1f3cc43d6d1a0f13f434a16bc
Dima Sukhariev <dmitriu.sy1303@gmail.com>
2026-04-28 12:44:44 +0300

    Added space

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list=None) -> None:
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
         if coords is None:
@@ -29,7 +29,7 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list=None) -> None:
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
         if coords is None:
             coords = [0, 0, 0]
 
